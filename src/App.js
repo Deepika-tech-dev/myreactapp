9b1d9d40382343a646677384479a16bb97fb73b2
Deepika-tech-dev <deepswarnstudy777@gmail.com>
2025-05-21 14:43:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fro skill developments
+          Learn React for skill developments
         </a>
       </header>
     </div>
